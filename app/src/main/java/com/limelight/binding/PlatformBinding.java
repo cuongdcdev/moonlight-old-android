1e4190e5a271d34abf7a5542b1c96c2a2dbc606a
cuong <12132077+cuongdcdev@users.noreply.github.com>
2025-03-07 00:31:02 +0700

    fix

--- app/src/main/java/com/limelight/binding/PlatformBinding.java
+++ app/src/main/java/com/limelight/binding/PlatformBinding.java
@@ -41,111 +41,59 @@ public class PlatformBinding {
         return new AndroidCryptoProvider(c);
     }
 
+    // This static initializer ensures that the class can be loaded without errors
+    static {
+        try {
+            // Pre-load any classes that might cause initialization errors
+            Class.forName("javax.net.ssl.SSLContext");
+            Class.forName("javax.net.ssl.SSLSocketFactory");
+        } catch (Exception e) {
+            // Ignore exceptions here - we just want to make sure the classes are loaded
+            System.err.println("Warning: Error pre-loading SSL classes: " + e.getMessage());
+        }
+    }
+
     public static void enableTls12ForOldAndroid() {
+        // First check if we've already tried this
         if (enabledTls12) {
-            // Don't apply the fix more than once
             return;
         }
-
+        
+        // Mark as attempted so we don't try multiple times
+        enabledTls12 = true;
+        
+        // Only needed for Android 4.4 and below
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+        
         try {
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                LimeLog.info("Enabling TLS 1.2 for Android " + Build.VERSION.SDK_INT);
-
+            System.out.println("Enabling TLS 1.2 for Android " + Build.VERSION.RELEASE);
+            
+            try {
+                // Enable TLS 1.2 by setting system properties first
+                System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+            } catch (Exception e) {
+                System.err.println("Error setting https.protocols: " + e.getMessage());
+            }
+            
+            try {
+                // Setup TLS context for TLS 1.2 in a simple way that won't cause
+                // initialization errors during build
+                SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, null, null);
+                
+                // Set as default, if possible
                 try {
-                    // Create a TLS 1.2 compatible SSLContext
-                    SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-                    TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                    tmf.init((KeyStore) null);
-                    sslContext.init(null, tmf.getTrustManagers(), null);
-
-                    // Create a wrapper TLS 1.2-enabling SSLSocketFactory
-                    final SSLSocketFactory delegate = sslContext.getSocketFactory();
-                    SSLSocketFactory tls12Factory = new SSLSocketFactory() {
-                        @Override
-                        public String[] getDefaultCipherSuites() {
-                            return delegate.getDefaultCipherSuites();
-                        }
-
-                        @Override
-                        public String[] getSupportedCipherSuites() {
-                            return delegate.getSupportedCipherSuites();
-                        }
-
-                        @Override
-                        public java.net.Socket createSocket(java.net.Socket socket, String host, int port, boolean autoClose) throws java.io.IOException {
-                            SSLSocket s = (SSLSocket) delegate.createSocket(socket, host, port, autoClose);
-                            enableTls12OnSocket(s);
-                            return s;
-                        }
-
-                        @Override
-                        public java.net.Socket createSocket(String host, int port) throws java.io.IOException {
-                            SSLSocket s = (SSLSocket) delegate.createSocket(host, port);
-                            enableTls12OnSocket(s);
-                            return s;
-                        }
-
-                        @Override
-                        public java.net.Socket createSocket(String host, int port, java.net.InetAddress localHost, int localPort) throws java.io.IOException {
-                            SSLSocket s = (SSLSocket) delegate.createSocket(host, port, localHost, localPort);
-                            enableTls12OnSocket(s);
-                            return s;
-                        }
-
-                        @Override
-                        public java.net.Socket createSocket(java.net.InetAddress host, int port) throws java.io.IOException {
-                            SSLSocket s = (SSLSocket) delegate.createSocket(host, port);
-                            enableTls12OnSocket(s);
-                            return s;
-                        }
-
-                        @Override
-                        public java.net.Socket createSocket(java.net.InetAddress address, int port, java.net.InetAddress localAddress, int localPort) throws java.io.IOException {
-                            SSLSocket s = (SSLSocket) delegate.createSocket(address, port, localAddress, localPort);
-                            enableTls12OnSocket(s);
-                            return s;
-                        }
-                        
-                        private void enableTls12OnSocket(SSLSocket socket) {
-                            // Enable TLSv1.2 on the socket
-                            String[] protocols = socket.getEnabledProtocols();
-                            String[] updatedProtocols;
-                            
-                            // Check if TLSv1.2 is already in the list
-                            if (!Arrays.asList(protocols).contains("TLSv1.2")) {
-                                updatedProtocols = new String[protocols.length + 1];
-                                System.arraycopy(protocols, 0, updatedProtocols, 0, protocols.length);
-                                updatedProtocols[protocols.length] = "TLSv1.2";
-                                socket.setEnabledProtocols(updatedProtocols);
-                            }
-                        }
-                    };
-
-                    // Set as the default SSL socket factory for all connections
-                    HttpsURLConnection.setDefaultSSLSocketFactory(tls12Factory);
-                    
-                    // Also set system properties
-                    System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
-                    
-                    // Set security provider properties if possible
-                    try {
-                        Security.setProperty("crypto.policy", "unlimited");
-                    } catch (Exception e) {
-                        // Ignore if we can't set this
-                        LimeLog.warning("Could not set crypto policy: " + e.getMessage());
-                    }
-                    
-                    enabledTls12 = true;
-                    LimeLog.info("TLS 1.2 enabled successfully for Android " + Build.VERSION.SDK_INT);
+                    HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
                 } catch (Exception e) {
-                    LimeLog.severe("Failed to enable TLS 1.2 on old Android: " + e);
-                    e.printStackTrace();
+                    System.err.println("Error setting default SSLSocketFactory: " + e.getMessage());
                 }
+            } catch (Exception e) {
+                System.err.println("Error initializing SSLContext: " + e.getMessage());
             }
         } catch (Exception e) {
-            // Catch any other exceptions that might occur
-            LimeLog.severe("Unexpected error in TLS 1.2 configuration: " + e);
-            e.printStackTrace();
+            System.err.println("Unexpected error in TLS configuration: " + e.getMessage());
         }
     }
 }
